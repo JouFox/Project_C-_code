20b7e374bdceecc9b4ceee1666a707fbe4fb080a
JouFoix <logosf0x080@gmail.com>
2022-12-18 23:25:17 +1000

    Выполнение практической работы 2 семнара от 18.12.2022

---
+++ Code_005.1_Week/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+int need = day;
+int week = 0;
+while (need > 7)
+{
+    need = need - 7;
+    week = week + 1;
+}
+if      (day % 7 == 0)              Console.WriteLine($"Число {day} экваволентное воскресению является выходным");
+else if ((day - week) % 6 == 0)     Console.WriteLine($"Число {day} экваволентное субботе является выходным ");
+else if (day < 0 )                  Console.WriteLine($"Число {day} вышло за пределы допустимых значений");
+else                                Console.WriteLine($"Число {day} не является выходным ");
+                               
---
+++ Code_005/Program.cs
@@ -0,0 +1,32 @@
+﻿int Forse(int leg) // Функция подсчёта разрядов
+{ 
+    var count = 0;
+
+    while (leg > 0)
+    {
+        leg = leg / 10;
+        count++;
+    }
+    count = count - 3;
+    return count;
+}
+
+Console.Write("Ведите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)   Console.WriteLine($"Третья цифра числа {a}, не найдена ");
+
+else if (a < 1000) 
+{
+    Console.WriteLine("Вторая цифра числа " + a + " = " + a / 10 % 10); // Блок 1 Задания
+    Console.WriteLine("Третья цифра числа " + a + " = " + a % 10);
+}
+else // Блок 2 Задания
+{
+    int col = Forse(a);
+    int i = Convert.ToInt32(Math.Pow(10, col));
+    int x = a / i % 10;
+    Console.WriteLine($"Третья цифра числа {a} = {x}");
+}
+
+
+
