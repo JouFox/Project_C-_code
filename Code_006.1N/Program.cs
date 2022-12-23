0a9c9642cd09a0922fe2d8e8e5b5f6c8983bd060
JouFoix <logosf0x080@gmail.com>
2022-12-24 08:14:17 +1000

    Решение задач 19, 23 в Семинаре 3

---
+++ Code_006.1N/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ведите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int cost = 0, secret;
+if (n == 0) Console.WriteLine("Число N не может быть меньше или равно нулю");
+while ((cost < n) & (n > 0)) 
+        {
+        cost++;
+        secret = cost * cost * cost;
+        Console.WriteLine(secret);
+        }
+    
---
+++ Code_006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ведите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] dis = new int[4];
+if ((((999 < a) & (a > 0)) | ((-999 > a) & (a < 0)) && (((a < 100000) & (a > 0)) | ((a > -1000000) & (a < 0)))))
+{
+    dis[3] = a % 10;
+    dis[2] = a / 10 % 10;
+    dis[1] = a / 1000 % 10;
+    dis[0] = a / 10000;
+    if ((dis[0] == dis[3]) && (dis[1] == dis[2])) Console.WriteLine($"Число {a} является палиндромом");
+    else Console.WriteLine($"Число {a} является не палиндромом");
+}
+else Console.WriteLine($"Введённое чесло {a} выходит за допустимую область"); 
