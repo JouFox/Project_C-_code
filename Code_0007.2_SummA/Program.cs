2f242f0eaa57452d963019782e589fae96e6a4b4
JouFoix <logosf0x080@gmail.com>
2023-01-15 00:54:46 +1000

    Решение задачь семинара 14.01.23

---
+++ Code_0007.2_SummA/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.Write("Введите число: ");                   //Ввод числа
+string B = Console.ReadLine();
+int vara, summ = 0;
+int leng = B.Length;                                //Нахождение длины
+for (int i = 0; i < leng; i++)                      //Преобразование нахождение суммы цифр числа   
+{
+    if (B[i] == '-') 
+    { 
+        vara = int.Parse(Convert.ToString(B[i+1]));
+        summ = summ - vara;
+        i++;
+    }
+    else
+    {
+        vara = int.Parse(Convert.ToString(B[i]));
+        summ = summ + vara;                     
+    } 
+   
+}
+Console.WriteLine($"Сумма чисел {B} = {summ}");      //Вывод
